df8f2dbca649a2d95410f5d362d2d53c42bd82ad
Jeremy D'silva <jeremyd2000@hotmail.com>
2025-12-26 14:15:05 -0500

    Added LeftClick logic

--- src/main/kotlin/gg/mino/minocore/items/MinoCustomItem.kt
+++ src/main/kotlin/gg/mino/minocore/items/MinoCustomItem.kt
@@ -1,11 +1,15 @@
 package gg.mino.minocore.items
 
+import gg.mino.minocore.player.MinoPlayer
 import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
+import sun.net.www.content.text.plain
 
 abstract class MinoCustomItem(val id: String) {
     abstract fun getItemStack(): ItemStack
     abstract fun onRightClick(player: Player)
+
+    abstract fun onLeftClick(player: Player)
 
     fun isInstance(item: ItemStack): Boolean {
         return item.getMinoTag("mino_item_id") == id
